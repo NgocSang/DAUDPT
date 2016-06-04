32a8eac5f965b3ea2b3f648649f6b21434e861fb
Nguyen Ngoc Sang <1312485@student.hcmus.edu.vn>
2016-06-04 15:42:38 +0700

    Thiết kế giao diện các trang -Home -Store -Detail -Contact Thiết kế theo phần reponsive

---
+++ config/routes.js
@@ -0,0 +1,120 @@
+
+/**
+ * Module dependencies.
+ */
+
+
+var passport = require('passport');
+
+var mongoose = require('mongoose');
+var login = require('login');
+var signup = require('signup');
+var profile = require('profile');
+var User = mongoose.model('User');
+/**
+ * Expose
+ */
+
+module.exports = function (app, passport) {
+
+
+  app.get('/signup', signup.index);
+
+  app.get('/home',function(req, res, next){
+    res.render('home');
+  });
+  app.get('/store',function(req, res, next){
+    res.render('store');
+  });
+  app.get('/contact',function(req, res, next){
+    res.render('contact');
+  });
+  app.get('/details',function(req, res, next){
+    res.render('details');
+  });
+/*app.get('/', function(req, res) {
+ // hiển thị view login và login message Mnếu nó tồn tại.
+ res.render('profile', { message: req.flash('loginMessage') });
+ });
+
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+    user = req.user;
+
+    ///----------------------
+
+
+
+    ////--------------------
+
+  res.render('profile', {
+    user : req.user
+    // lấy thông tin người dùng trong session
+    //và truyền qua view
+    });
+  });
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+  // route middleware kiểm tra để chắc chắn là
+    // người dùng đã đăng nhập
+    function isLoggedIn(req, res, next) {
+
+    // nếu người dùng đã đăng nhập thì tiếp tục thực hiện
+     if (req.isAuthenticated())
+     return next();
+
+    // ngược lại điều hướng về đăng nhập.
+     res.redirect('/');
+    }
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+// Xử lý sau khi facebook xác thực thành viên
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect : '/profile',
+    failureRedirect : '/login'
+  }));
+
+
+  app.post('/signup', passport.authenticate('signup', {
+ successRedirect : '/profile',
+ // Chuyển về trang thông tin cá nhân nếu đăng ký thành công
+ failureRedirect : '/signup',
+ // Điều hướng về lại trang đăng ký nếu có lỗi
+ failureFlash : true // cho phép flash messages
+ }));
+
+  app.post('/profile', passport.authenticate('login', {
+ successRedirect : '/profile',
+ // Chuyển về trang thông tin cá nhân nếu đăng ký thành công
+ failureRedirect : '/',
+ // Điều hướng về lại trang đăng nhập nếu có lỗi
+ failureFlash : true // cho phép flash messages
+ }));
+  /**
+   * Error handling
+   */
+
+  app.use(function (err, req, res, next) {
+    // treat as 404
+    if (err.message
+      && (~err.message.indexOf('not found')
+      || (~err.message.indexOf('Cast to ObjectId failed')))) {
+      return next();
+    }
+    console.error(err.stack);
+    // error page
+    res.status(500).render('500', { error: err.stack });
+  });
+
+  // assume 404 since no middleware responded
+  app.use(function (req, res, next) {
+    res.status(404).render('404', {
+      url: req.originalUrl,
+      error: 'Not found'
+    });
+  });
+
+
+};
