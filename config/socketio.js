c1b59b26e7c11ae9b736446c91c8c5252444bc2c
Nguyen Ngoc Sang <1312485@student.hcmus.edu.vn>
2016-06-10 21:24:21 +0700

    Tạo sechema cho dữ liệu product. Load dữ liệu tử mongodb lên trang web.

---
+++ config/socketio.js
@@ -0,0 +1,53 @@
+var login = require('../config/routes');
+var mang = [];
+module.exports = function (app, io) {
+  function pushmang(mang, data){
+    var check = mang.indexOf(data);
+    if(check == -1)
+    {
+      mang.push(data);
+
+    }
+  }
+io.on('connection', function(socket) {
+  //console.log('A user connected');
+  socket.on('user info', function(data) {
+    socket.userId = data.userId;
+    if (data.userNickname == socket.userNickname) {
+      return;
+    }
+  //console.log("socket");
+    //console.log(user);
+    var welcomeMsg = (!socket.userNickname) ? data.userNickname + ' is online' : socket.userNickname + ' renamed to ' + data.userNickname;
+    socket.userNickname = data.userNickname;
+    if(socket.userNickname != "admin")
+    {
+      pushmang(mang,socket.userNickname);
+    }
+
+    io.emit('name message', welcomeMsg);
+    if (socket.userNickname == 'admin' || socket.userNickname == mang[0]) {
+      socket.join('room1');
+    }
+  });
+
+  socket.on('chat message', function(data) {
+    //console.log('msg: ' + JSON.toString(data));
+    data.userId = socket.userId;
+    data.userNickname = socket.userNickname;
+
+    if (socket.userNickname == 'admin' || socket.userNickname == mang[0]) {
+      io.in('room1').emit('chat message', data);
+      return;
+    }
+  });
+
+  /*socket.on('disconnect', function() {
+      console.log('Got disconnect!');
+
+      var i = allClients.indexOf(socket);
+      allClients.splice(i, 1);
+   });*/
+});
+
+}
---
+++ public/js/details.js
@@ -0,0 +1,22 @@
+(function($) {
+    $(function() {
+        
+        $('#zoom_01').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 300,
+zoomWindowFadeOut: 750
+   }); 
+     //initiate the plugin and pass the id of the div containing gallery images
+    $("#zoom_01").elevateZoom({
+     gallery: 'gal1', 
+     cursor: 'pointer', 
+     galleryActiveClass: 'active', 
+     imageCrossfade: true, 
+     loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif' 
+ });
+    //pass the images to Fancybox
+    $("#zoom_01").bind("click", function (e) { var ez = $('#zoom_01').data('elevateZoom'); $.fancybox(ez.getGalleryList()); return false; });
+
+    })
+})(jQuery);
