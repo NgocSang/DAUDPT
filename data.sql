77e813484347e033aa2f605bbac4a63a2e224f4c
NgocSang <1312485@student.hcmus.edu.vn>
2016-06-29 21:48:07 +0700

    Thêm file data

---
+++ data.sql
@@ -0,0 +1,484 @@
+{
+    "productID" : "SP004",
+    "basicInfo" : {
+        "imgUrl" : "http://cdn1.yame.vn/pimg/so-mi-nam-kirimaru-h01-0012351/9a926efc-a1d1-0100-dc28-00124b7939d9?w=440",
+        "name" : "RVCA Sucker Punch 2 Mauve Swing Dress",
+        "price" : 500000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://cdn1.yame.vn/pimg/so-mi-nam-kirimaru-h01-0012351/9a926efc-a1d1-0100-dc28-00124b7939d9?w=440", 
+            "http://cdn1.yame.vn/pimg/so-mi-nam-kirimaru-h01-0012351/4d33a138-8791-0200-ed8a-00124b7968f8?w=800", 
+            "http://cdn1.yame.vn/pimg/so-mi-nam-kirimaru-h01-0012351/39a19f12-609b-0300-9718-00124b7968fc?w=800", 
+            "http://cdn1.yame.vn/pimg/so-mi-nam-kirimaru-h01-0012351/f0cef5cc-03be-0500-26e0-00124b796903?w=800"
+        ]
+    },
+    "color" : [ 
+        "Green", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S"
+    ],
+    "featured" : true
+}
+-----------------------------------------------------------
+{
+    "productID" : "SP005",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/8t2zvY",
+        "name" : "Riviera Resort Ivory Lace Dress",
+        "price" : 500000,
+        "tag" : "Dress",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/8t2zvY",
+            "https://goo.gl/7SFU7A"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S"
+    ],
+    "featured" : true
+}
+
+---------------------------------------------------------------
+
+{
+    "productID" : "SP006",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/qQFIHB",
+        "name" : "Sport shoes",
+        "price" : 500000,
+        "tag" : "Shoe",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/qQFIHB",
+            "https://goo.gl/gqT8l8",
+            "https://goo.gl/p20LKi"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S"
+    ],
+    "featured" : false
+}
+----------------------------------------------------------------------
+{
+    "productID" : "SP007",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/uYOwpc",
+        "name" : "Shirt",
+        "price" : 250000,
+        "tag" : "Clother",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/EujqsR",
+            "https://goo.gl/uYOwpc"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+
+-----------------------------------------------------------------------
+
+{
+    "productID" : "SP008",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/E14wXR",
+        "name" : "Shirt",
+        "price" : 250000,
+        "tag" : "Clother",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/E14wXR",
+	    "https://goo.gl/YCQsTg"
+            
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+--------------------------------------------------------------------------
+{
+    "productID" : "SP009",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/hkucjq",
+        "name" : "Hat",
+        "price" : 250000,
+        "tag" : "Clother",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/7SFU7A",
+            "https://goo.gl/hkucjq"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White",
+	"Pink",
+	"Brown"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+
+
+-----------------------------------------------------------------------------------------
+{
+    "productID" : "SP010",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/IF1pP5",
+        "name" : "Shoe for girl",
+        "price" : 250000,
+        "tag" : "Shoe",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/IF1pP5",
+            "https://goo.gl/39SsP1"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White",
+	"Pink",
+	"Brown"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+---------------------------------------------------------------------
+
+
+{
+    "productID" : "SP011",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/oWcXZX",
+        "name" : "Clother for men",
+        "price" : 225000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/YVr7bb",
+            "https://goo.gl/oWcXZX"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+---------------------------------------------------------------------
+{
+    "productID" : "SP012",
+    "basicInfo" : {
+        "imgUrl" : "https://goo.gl/HIw3mA",
+        "name" : "Shirts CS Combined 9",
+        "price" : 225000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "https://goo.gl/HIw3mA",
+            "https://goo.gl/228UXA",
+            "https://goo.gl/puVLN3"
+        ]
+    },
+    "color" : [ 
+        "Black", 
+        "White"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+-----------------------------------------------------------------------------------------
+{
+    "productID" : "SP013",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/eFGiL0",
+        "name" : "Shirts CS Combined 8",
+        "price" : 225000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/RM8O9T",
+            "http://goo.gl/Feczld",
+            "http://goo.gl/q071Os",
+            "http://goo.gl/LcwJ4K"
+        ]
+    },
+    "color" : [ 
+        "Blue", 
+        "White",
+	"Pink"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+-------------------------------------------------------------------------
+{
+    "productID" : "SP014",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/rxw4I8",
+        "name" : "Shirts CS Combined 6",
+        "price" : 225000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/ZSYP04",
+            "http://goo.gl/6bKE4N",
+            "http://goo.gl/7qM9TG"
+        ]
+    },
+    "color" : [ 
+        "Blue", 
+        "White",
+	"Pink"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+
+------------------------------------------------------------------------------
+
+{
+    "productID" : "SP015",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/MYCwCS",
+        "name" : "Blue Life Festival Blue Grey and Ivory Tie-Dye Skort Sweater",
+        "price" : 225000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/IKqRcb",
+            "http://goo.gl/KOmjj6",
+            "http://goo.gl/MYCwCS"
+            
+        ]
+    },
+    "color" : [ 
+        "Blue", 
+        "White",
+	"Brown"
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+--------------------------------------------------------------------------------------
+
+{
+    "productID" : "SP016",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/12XjI2",
+        "name" : "Calligraphy Fonts Blue and Ivory Print Skater Sweater",
+        "price" : 390000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/12XjI2",
+            "http://goo.gl/ngs0HF",
+            "http://goo.gl/slrS24",
+            "http://goo.gl/mnGbdZ"
+        ]
+    },
+    "color" : [  
+        "White"
+	
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+---------------------------------------------------------------------------------------------------
+
+{
+    "productID" : "SP017",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/tL276t",
+        "name" : "Shirts CS Combined 13 for man",
+        "price" : 390000,
+        "tag" : "Shirts",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/U6WnQL",
+            "http://goo.gl/cYJqOq"
+        
+        ]
+    },
+    "color" : [  
+        "White",
+	"Blue"
+	
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : true
+}
+
+-------------------------------------------------------------------------------------------
+{
+    "productID" : "SP018",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/IvswIe",
+        "name" : "Fox Hill Black Halter Maxi Sweater",
+        "price" : 390000,
+        "tag" : "Shoe",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/IvswIe",
+            "https://goo.gl/EujqsR",
+            "http://goo.gl/7NjezB",
+            "http://goo.gl/TbvrAV"
+        ]
+    },
+    "color" : [  
+        "White-Blue",
+	"White-Green"
+	
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+------------------------------------------------------------------------------------------------
+
+{
+    "productID" : "SP019",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/CycJya",
+        "name" : "Lost Coastlines Blue Tie-Dye Maxi ",
+        "price" : 390000,
+        "tag" : "Sweater",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/CycJya",
+            "http://goo.gl/11mfYD",
+            "http://goo.gl/8HVvp7"
+        
+        ]
+    },
+    "color" : [  
+        "White",
+	"Black",
+	"Blue",
+	"Red"
+	
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
+-----------------------------------------------------------------------------------------------
+{
+    "productID" : "SP020",
+    "basicInfo" : {
+        "imgUrl" : "http://goo.gl/p8cnZG",
+        "name" : "All the Sway Convertible Light Grey Maxi Sweater",
+        "price" : 390000,
+        "tag" : "Sweater",
+        "description" : "Inspired by the iconic artist/poet, Shel Silverstein, and his books we read as kids, this graphic is a direct reference to his story, but in the context of a lost Adam Bomb in search of peace. Comes on a 5.5oz 100% cotton tubular jersey with double-needle bottom hemming and sleeves, shoulder-to-shoulder tape, and arrives preshrunk to minimize shrinkage. Available in S-2XL. Available in S-2XL. Available in Black, Mint, and Royal",
+        "listimgUrl" : [ 
+            "http://goo.gl/p8cnZG",
+            "http://goo.gl/rnexib",
+	    "http://goo.gl/hJTeqS"
+        
+        ]
+    },
+    "color" : [  
+        "White-Pink",
+	"While-Blue"
+	
+    ],
+    "size" : [ 
+        "XL", 
+        "M", 
+        "S",
+	"L"
+    ],
+    "featured" : false
+}
